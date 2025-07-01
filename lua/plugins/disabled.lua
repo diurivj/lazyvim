7ec2e2f2418e3e7324a9f598bf7eb75e79f763bb
diegovazquez <diegovazquez@etsy.com>
2025-07-01 16:58:37 +0000

    fixing intelephense config

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -28,4 +28,45 @@ return {
       },
     },
   },
+
+  -- Lsp custom
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        intelephense = {
+          settings = {
+            intelephense = {
+              files = {
+                maxSize = 1000000,
+                exclude = {
+                  "**/.git/**",
+                  "**/.svn/**",
+                  "**/.hg/**",
+                  "**/CVS/**",
+                  "**/.DS_Store/**",
+                  "**/node_modules/**",
+                  "**/bower_components/**",
+                  "**/htdocs_customshops/**",
+                  "**/htdocs_gearman/**",
+                  "**/htdocs/assets/dist/**",
+                  "**/tmp/**",
+                  "**vendor/*/{!(phpunit)/**}",
+                  "translations/**",
+                  "**/.phan/**",
+                  "**/cron.d/**",
+                  "**/generated/**",
+                  "**/Generated/**",
+                  "**/modules/css/**",
+                  "**/__modules__*__src__/**",
+                  "vendor/etsy/module-*/**",
+                },
+              },
+              maxMemory = 10192,
+            },
+          },
+        },
+      },
+    },
+  },
 }
